ee1b4fba236314de49b4163b50bf68edc2e753b9
hasanarcas <hasanarcas@gmail.com>
2021-11-14 21:31:46 +0300

    Created "Main page" and "Normal Game" page

---
+++ Name-City-Game/app/src/main/java/tr/edu/mu/ceng/mad/name_city_game/NormalGameActivity.java
@@ -0,0 +1,14 @@
+package tr.edu.mu.ceng.mad.name_city_game;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NormalGameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_normal_game);
+    }
+}
