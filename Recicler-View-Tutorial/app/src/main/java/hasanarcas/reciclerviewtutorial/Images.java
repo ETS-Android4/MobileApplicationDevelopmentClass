abb7ecf167f087af3b96bf382ead2a55cf2156f2
hasanarcas <hasanarcas@gmail.com>
2021-11-18 19:48:22 +0300

    Created image_item.xml and Images.java

---
+++ Recicler-View-Tutorial/app/src/main/java/hasanarcas/reciclerviewtutorial/Images.java
@@ -0,0 +1,5 @@
+package hasanarcas.reciclerviewtutorial;
+
+public class Images {
+
+}
--- Recicler-View-Tutorial/app/src/main/java/hasanarcas/reciclerviewtutorial/MainActivity.java
+++ Recicler-View-Tutorial/app/src/main/java/hasanarcas/reciclerviewtutorial/MainActivity.java
@@ -1,14 +1,18 @@
 package hasanarcas.reciclerviewtutorial;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView mRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mRecyclerView = findViewById(R.id.main_activity_reciclerView);
     }
 }
