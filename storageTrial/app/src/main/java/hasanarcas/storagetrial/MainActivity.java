d1f91d262512adb6bca351e3c56ca23a33cf5197
hasanarcas <hasanarcas@gmail.com>
2021-12-22 22:55:44 +0300

    commit

---
+++ storageTrial/app/src/main/java/hasanarcas/storagetrial/MainActivity.java
@@ -0,0 +1,36 @@
+package hasanarcas.storagetrial;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+public class MainActivity extends AppCompatActivity {
+    ImageView image;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        image = findViewById(R.id.imageView);
+
+        FirebaseStorage storage = FirebaseStorage.getInstance();
+        StorageReference storageRef = storage.getReference();
+        StorageReference image1 = storageRef.child("new_released_games/ archvale .jpg");
+
+        Glide.with(this ).load(image1).into(image);
+
+    }
+}
