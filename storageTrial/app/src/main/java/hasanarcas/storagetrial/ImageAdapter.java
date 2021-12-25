d699be4dad4f29883ee55bbd360ae1f072cc2286
hasanarcas <hasanarcas@gmail.com>
2021-12-25 22:21:20 +0300

    Created Firestore app and added all the games features in it

--- storageTrial/app/src/main/java/hasanarcas/storagetrial/ImageAdapter.java
+++ storageTrial/app/src/main/java/hasanarcas/storagetrial/ImageAdapter.java
@@ -21,44 +21,6 @@ import com.bumptech.glide.Glide;
 import java.util.ArrayList;
 import java.util.zip.Inflater;
 
-/*public class ImageAdapter extends BaseAdapter {
-    Context context;
-    ArrayList<Integer> list = new ArrayList<>();
-    ImageView image;
-    LayoutInflater inflater;
-
-    public ImageAdapter(Context context, ArrayList<Integer> list) {
-        this.context = context;
-        this.list = list;
-        inflater = (LayoutInflater.from(context));
-    }
-
-    @Override
-    public int getCount() {
-        return list.size();
-    }
-
-    @Override
-    public Object getItem(int i) {
-        return null;
-    }
-
-    @Override
-    public long getItemId(int i) {
-        return 0;
-    }
-
-    @Override
-    public View getView(int i, View view, ViewGroup viewGroup) {
-        view = inflater.inflate(R.layout.image_list, null);
-        ImageView new_image = (ImageView) view.findViewById(R.id.image_list_image);
-        new_image.setImageResource(list.get(i));
-        return view;
-    }
-}*/
-
-
-
 public class ImageAdapter extends RecyclerView.Adapter<ImageAdapter.ViewHolder> {
 
     private ArrayList<String> imageList;
--- storageTrial/app/src/main/java/hasanarcas/storagetrial/MainActivity.java
+++ storageTrial/app/src/main/java/hasanarcas/storagetrial/MainActivity.java
@@ -9,10 +9,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.GridView;
 import android.widget.ProgressBar;
-import android.widget.TextView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -23,93 +20,17 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.ListResult;
 import com.google.firebase.storage.StorageReference;
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvValidationException;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-import java.util.concurrent.TimeUnit;
-
-/*
-public class MainActivity extends AppCompatActivity {
-    GridView grid;
-    ArrayList<Integer> myList = new ArrayList<>();
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        grid = findViewById(R.id.grid);
-
-        */
-/*myList.add(R.drawable.ic_launcher_background);
-        myList.add(R.drawable.common_google_signin_btn_text_dark_focused);*//*
-
-
-
-        FirebaseStorage storage = FirebaseStorage.getInstance();
-        StorageReference storageRef = storage.getReference();
-
-
-        */
-/*StorageReference image1 = storageRef.child("games/archvale.jpg");
-        final long ONE_MEGABYTE = 1024 * 1024;
-        image1.getBytes(ONE_MEGABYTE).addOnSuccessListener(new OnSuccessListener<byte[]>() {
-            @Override
-            public void onSuccess(byte[] bytes) {
-                Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
-                image.setImageBitmap(bitmap);
-            }
-        });
-*//*
-
-
-        ImageAdapter adapter = new ImageAdapter(this, myList);
-        grid.setAdapter(adapter);
-
-
-
-        */
-/*FirebaseFirestore firestore = FirebaseFirestore.getInstance();
-        DocumentReference ref = firestore.collection("games").document("game_properties");
-        ref.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                if(task.isSuccessful()){
-                    DocumentSnapshot document = task.getResult();
-                    if(document.exists()){
-                        String text = document.getString("content");
-                        Scanner sc = new Scanner(text);
-                        sc.useDelimiter(",");   //sets the delimiter pattern
-                        while (sc.hasNext())  //returns a boolean value
-                        {
-                            Log.d("11111" , sc.next());  //find and returns the next complete token from this scanner
-                        }
-
-                    }
-                    else{
-                        Log.d("1111111", "no document..........");
-                    }
-                }
-                else{
-                    Log.d("11111111", "get failed with " + task.getException());
-                }
-            }
-        });*//*
-
-
-    }
-
-
-}*/
-
-
-
-
 
 public class MainActivity extends AppCompatActivity {
     ArrayList<String> imagelist;
     RecyclerView recyclerView;
-    StorageReference root;
     ProgressBar progressBar;
     ImageAdapter adapter;
     @Override
@@ -123,6 +44,47 @@ public class MainActivity extends AppCompatActivity {
         progressBar=findViewById(R.id.progress);
         progressBar.setVisibility(View.VISIBLE);
         StorageReference listRef = FirebaseStorage.getInstance().getReference().child("new_released_games");
+        download(listRef);
+
+        FirebaseFirestore firestore = FirebaseFirestore.getInstance();
+        DocumentReference ref = firestore.collection("games").document("game_properties");
+        ArrayList<String[]> list = new ArrayList<>();
+        ref.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if(task.isSuccessful()){
+                    DocumentSnapshot document = task.getResult();
+                    if(document.exists()){
+                        String text = document.getString("content");
+                        try {
+                            CSVReader csvReader = new CSVReader(new FileReader(text));
+                            while(csvReader.readNext() != null){
+                                list.add(csvReader.readNext());
+                            }
+                        } catch (FileNotFoundException e) {
+                            e.printStackTrace();
+                        } catch (CsvValidationException e) {
+                            e.printStackTrace();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    else{
+                        Log.d("1111111", "no document..........");
+                    }
+                }
+                else{
+                    Log.d("11111111", "get failed with " + task.getException());
+                }
+                for(String[] e : list){
+                    Log.d("111111111", String.valueOf(e));
+                    //System.out.println(e);
+                }
+            }
+        });
+    }
+
+    public void download(StorageReference listRef){
         listRef.listAll().addOnSuccessListener(new OnSuccessListener<ListResult>() {
             @Override
             public void onSuccess(ListResult listResult) {
@@ -132,7 +94,7 @@ public class MainActivity extends AppCompatActivity {
                         public void onSuccess(Uri uri) {
                             if(uri.toString().contains("demeo")){
                                 imagelist.add(uri.toString());
-                                Log.e("Itemvalue",uri.toString());
+                                Log.e("Itemvalue","" + uri.toString());
                             }
 
                         }
